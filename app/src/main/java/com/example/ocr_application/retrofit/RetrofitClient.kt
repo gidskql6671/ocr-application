dcd488dae38de613b0d5a02e15723e7fef758529
gidskql6671 <gidskql6671@gmail.com>
2023-04-13 11:38:42 +0900

    네트워크 설정

--- app/src/main/java/com/example/ocr_application/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/ocr_application/retrofit/RetrofitClient.kt
@@ -1,19 +1,29 @@
 package com.example.ocr_application.retrofit
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
 
 object RetrofitClient {
-    private const val host = "http://34.64.157.192/"
+    private const val host = "http://34.64.239.253/"
 
     fun getApiService(): RetrofitInterface = getInstance().create(RetrofitInterface::class.java)
 
     private fun getInstance(): Retrofit {
         val gson = GsonBuilder().setLenient().create()
 
+        val okHttpClient = OkHttpClient.Builder()
+            .connectTimeout(40, TimeUnit.SECONDS)
+            .readTimeout(40, TimeUnit.SECONDS)
+            .writeTimeout(40, TimeUnit.SECONDS)
+            .build()
+
         return Retrofit.Builder()
             .baseUrl(host)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
