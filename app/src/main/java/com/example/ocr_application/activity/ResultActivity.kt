339f0ce10637c6488cc087965e54849442916a6e
gidskql6671 <gidskql6671@gmail.com>
2023-05-25 10:04:04 +0900

    로딩바 추가

--- app/src/main/java/com/example/ocr_application/activity/ResultActivity.kt
+++ app/src/main/java/com/example/ocr_application/activity/ResultActivity.kt
@@ -3,6 +3,8 @@ package com.example.ocr_application.activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ocr_application.databinding.ActivityResultBinding
@@ -22,6 +24,7 @@ class ResultActivity : AppCompatActivity() {
     private lateinit var correctPercentTextView: TextView
     private lateinit var originTextView: TextView
     private lateinit var correctTextView: TextView
+    private lateinit var loadingFrame: FrameLayout
 
     private lateinit var binding: ActivityResultBinding
     private var imagePath: String = ""
@@ -35,6 +38,7 @@ class ResultActivity : AppCompatActivity() {
         correctPercentTextView = binding.correctPercentTextView
         originTextView = binding.originText
         correctTextView = binding.correctText
+        loadingFrame = binding.loadingFrame
 
         imagePath = intent.extras!!.getString("imagePath")!!
         val correctText = intent.extras!!.getString("correctText")
@@ -68,6 +72,7 @@ class ResultActivity : AppCompatActivity() {
                     if (response.isSuccessful.not()) {
                         originTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
                         correctTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
+                        loadingFrame.visibility = View.GONE
 
                         return
                     }
@@ -80,12 +85,14 @@ class ResultActivity : AppCompatActivity() {
                         correctPercentTextView.text = String.format("맞춤법 정답률은 %.2f%%입니다.", correctPercent)
                         originTextView.text = it.originString
                         correctTextView.text = it.correctString
+                        loadingFrame.visibility = View.GONE
                     }
                 }
 
                 override fun onFailure(call: Call<OcrResponse>, t: Throwable) {
                     originTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
                     correctTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
+                    loadingFrame.visibility = View.GONE
 
                     Log.e("dong_request", t.toString())
                 }
@@ -102,6 +109,7 @@ class ResultActivity : AppCompatActivity() {
                 override fun onResponse(call: Call<OcrResponse>, response: Response<OcrResponse>) {
                     if (response.isSuccessful.not()) {
                         originTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
+                        loadingFrame.visibility = View.GONE
 
                         return
                     }
@@ -113,11 +121,13 @@ class ResultActivity : AppCompatActivity() {
 
                         correctPercentTextView.text = String.format("맞춤법 정답률은 %.2f%%입니다.", correctPercent)
                         originTextView.text = it.originString
+                        loadingFrame.visibility = View.GONE
                     }
                 }
 
                 override fun onFailure(call: Call<OcrResponse>, t: Throwable) {
                     originTextView.text = "문장을 인식하는데 실패했습니다. 다시 시도해주세요."
+                    loadingFrame.visibility = View.GONE
 
                     Log.e("dong_request", t.toString())
                 }
